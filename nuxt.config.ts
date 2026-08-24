b6f849cd1ac346062b22178d0ac267a4b8bbe661
RomainNicolaon <nicolaon.romain@gmail.com>
2026-08-24 15:51:58 +0200

    feat: add favicon support to enhance branding and user experience

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,12 @@ export default defineNuxtConfig({
         },
       ],
       link: [
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         {
           rel: 'preconnect',
